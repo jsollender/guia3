d96e43dda84c43018eda6dc0d3d5867be041636d
jsollender <131376833+jsollender@users.noreply.github.com>
2023-05-15 14:44:22 -0300

    lloro
    no lo puedo correr en la biblioteca porque no tiene fortran. no tengo idea si funcan esas funciones

---
+++ programa_principal.f90
@@ -0,0 +1,91 @@
+program principal
+
+	use iso
+	use modulo_interpolacion_newton
+	use !no me acuerdo el nombre :(
+
+	implicit none
+	
+	!declaración de variables
+	
+	real(wp)					::	c, pc, func
+	integer (il)				::	n, i
+	real(wp), dimension(0:n)	::	x, fx
+	
+	interface
+		function func (x) result (fx)
+			implicit none
+			real(wp), intent (in)	::	x
+			real(wp), intent (out)	::	fx	
+			end function
+	
+		function gunc (x) result (fx)
+			implicit none
+			real(wp), intent (in)	::	x
+			real(wp), intent (out)	::	fx	
+			end function
+	end interface
+	
+	n = 1
+	
+	x(0) = 0._wp 	
+	x(1) = 0.6_wp
+	x(2) = 0.9_wp
+	
+	c = 0.45
+	
+!.-.-.-.--.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.	
+!Newton
+!.-.-.-.--.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.
+	
+	do i = 0, n
+		fx(i) = func(x(i))
+	end do
+	
+	call modulo_interpolacion_newton (n, x, fx, c, pc)
+	print (*,*) "el pol para f segun newton", pc
+	
+!.-.-.-.--.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.
+	
+	do i = 0, n
+		fx(i) = gunc(x(i))
+	end do
+	
+	call modulo_interpolacion_newton (n, x, fx, c, pc)
+	print (*,*) "el pol para g segun newton", pc
+	
+!.-.-.-.--.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.	
+!Lagrange
+!.-.-.-.--.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.
+	
+	do i = 0, n
+		fx(i) = func(x(i))
+	end do
+	
+	call InserteNombreDelModuloDeLagrange (n, x, fx, c, pc)
+	print (*,*) "el pol para f segun Lagrange", pc
+	
+!.-.-.-.--.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.
+	
+	do i = 0, n
+		fx(i) = gunc(x(i))
+	end do
+	
+	call InserteNombreDelModuloDeLagrange (n, x, fx, c, pc)
+	print (*,*) "el pol para g segun Lagrange", pc
+	
+end program principal
+
+function func (x) result (fx)
+	implicit none
+	real(wp), intent (in)	::	x
+	real(wp), intent (out)	::	fx	
+	fx = log(x + 1)
+end function
+
+function gunc (x) result (fx)
+	implicit none
+	real(wp), intent (in)	::	x
+	real(wp), intent (out)	::	fx	
+	fx = sqrt(x + 1)
+end function
